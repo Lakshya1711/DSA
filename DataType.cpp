3454ee953f31c070a93916f0f6b2ef1463ff222a
Lakshya1711 <91628618+Lakshya1711@users.noreply.github.com>
2023-01-08 22:40:41 +0530

    Add files via upload

---
+++ DataType.cpp
@@ -0,0 +1,60 @@
+/*Data type 
+int(integer)--> 4 byte memory  32 bit
+char(character)--> 1 byte memory   8 bit
+bool(boolen)--> 1 bit     (True or False)
+float(Float)--> 8 byte
+double--> 8 byte
+
+variable never start with No.
+
+we can know the size of the variable by usinf sizeof().
+
+Type Casting convert one data type into another.
+
+How negative no. are stored 
+first bit tells
++ve--> 0
+-ve--> 1
+
+for taking only positive no. use 
+unsigned int 
+range 0--2^32-1
+
+Operator:
++,-,*,/
+relational operator
+==,<,>,<=,>=,!=
+Assignment operator
+=
+logical operator
+&&,||,!
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+//	int a=123;
+//	cout<<a<<endl;
+//	char b='v';
+//	cout<<b<<endl;
+//	bool b1=false;  //if true then it will print 1 for False 0
+//	cout<<b1<<endl;
+//	float n=1.2;
+//	cout<<n<<endl;
+//	double d=9.8;
+//	cout<<d<<endl;
+//	
+//	int size = sizeof(d);
+//	cout<<"Size of a is :"<<size<<endl;
+
+//int a='a';
+//cout<<a<<endl;
+//char ch=98;
+//cout<<ch<<endl;
+//char ch1=123456;
+//cout<<ch1<<endl;
+
+//unsigned int a=112;
+//cout<<a<<endl;
+
+}
