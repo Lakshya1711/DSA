8155d652538ea5ef0c9a608972e9518c5d54e8ae
Lakshya1711 <91628618+Lakshya1711@users.noreply.github.com>
2023-01-08 23:16:55 +0530

    Add files via upload
    Programs of IF and ELse

---
+++ Conditional.cpp
@@ -0,0 +1,60 @@
+/* 
+cin.get     used to read the value of special character
+int a,b;
+a=cin.get();
+cout<<"Value of a and b is: "<<a<<endl;
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+/*	
+	int n;
+	cin>>n;
+	
+	//cout<<"value of n is :"<<n<< endl;
+	
+	//postive
+	if(n>0){
+		cout<<"N is positive"<<endl;
+	}
+	else{
+		cout<<"N is negative"<<endl;
+	}
+*/
+
+/*tell +ve,-ve,0
+int a,b;
+cin>>a;
+cout<<"Value of a is: "<<a<<endl;
+
+if (a>0){
+	cout<<"a is positive";	
+}
+else if (a<0){
+	cout<<"a is negative";
+}
+else{
+	cout<<"a is 0";
+}
+*/
+
+/* tell uppercase, lowercase, numeric
+char ch;
+cin>>ch;
+cout<<"Input value of ch is:"<<ch<<endl;
+
+if(ch>='a'&&ch<='z'){
+	cout<<"LowerCase"<<endl;
+}
+else if(ch>='A'&&ch<='Z'){
+	cout<<"UpperCase"<<endl;
+}
+else if(ch>='0'&&ch<='9'){
+	cout<<"Numeric"<<endl;
+}
+else{
+	cout<<"Special Character";
+}
+*/
+}
