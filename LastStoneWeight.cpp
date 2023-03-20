12506fa51586e49eb3c657e4e27521c114283534
Lakshya1711 <Lucky17sh@gmail.com>
2023-03-21 00:19:13 +0530

    no updates

---
+++ LastStoneWeight.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Enter Element"<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
